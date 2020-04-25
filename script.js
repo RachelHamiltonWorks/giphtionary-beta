ce9160227f81141829ca29c2b7748c6130064a6d
Coop <61427314+codelucid@users.noreply.github.com>
2020-04-24 20:18:02 -0400

    update to footer, second html page, save button to html 2

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function searchDictionaryAndGiphy(headword) {
 
     // querying m-w api for the user input headword
     var queryURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + headword + "?key=edeb60ac-21d3-4109-93d4-16ae042e92ec";
-    
     $.ajax({
       url: queryURL,
       method: "GET"
@@ -12,11 +11,11 @@ function searchDictionaryAndGiphy(headword) {
     // printing the entire object to console
       console.log(responseMW);
     // variable for searched word
-    var searchedWord = $('<h3>').text(responseMW[0].meta.id);
-    console.log(responseMW[0].meta.id);
+    var searchedWord = $('<h4>').text(responseMW[0].hwi.hw);
+    console.log(responseMW[0].hwi.hw);
     
     // How do you pronounce the word
-    var sayWord = $('<h5>').text(responseMW[0].hwi.prs[0].mw);
+    var sayWord = $('<h6>').text(responseMW[0].hwi.prs[0].mw);
     console.log(responseMW[0].hwi.prs[0].mw);
 
     // This grabs the first letter of the searched word
@@ -30,22 +29,71 @@ function searchDictionaryAndGiphy(headword) {
     console.log(responseMW[0].hwi.prs[0].sound.audio);
     
     // First definition of the word
-    var wordDefinition1 = $('<h5>').text("1--" + responseMW[0].shortdef[0]);
+    var wordDefinition1 = $('<h5>').text("1." + responseMW[0].shortdef[0]);
     console.log(responseMW[0].shortdef[0]);
     
     // Second definition of the word
-    var wordDefinition2 = $('<h5>').text("2--" + responseMW[0].shortdef[1]);
+    var wordDefinition2 = $('<h5>').text("2." + responseMW[0].shortdef[1]);
     console.log(responseMW[0].shortdef[1]);
 
-    $("#headword-entry").empty();
-    $("#headword-entry").append(searchedWord, sayWord, verbalWord, wordDefinition1, wordDefinition2)
+    $(".headword-entry").empty();
+    $(".headword-entry").append(searchedWord, sayWord, verbalWord, wordDefinition1, wordDefinition2)
     })
 
+
+
+
+
+
+
+
+    // var spanishURL = "https://www.dictionaryapi.com/api/v3/references/spanish/json/" + headword + "?key=39c727f0-8f59-4896-8532-707dfb1b035d";
+
+    // $.ajax({
+    //   url: spanishURL,
+    //   method: "GET"
+    // }).then(function(responseSpanish) {
+
+    // // printing the entire object to console
+    //   console.log("Spanish Response", responseSpanish);
+    // // variable for searched word
+    // var searchedSpanishWord = $('.test').text(responseSpanish[0].shortdef[0]);
+    // console.log("Spanish Word", searchedSpanishWord);
+    
+    // // How do you pronounce the word
+    // var sayWord = $('<h6>').text(responseSpanish[0].hwi.prs[0].mw);
+    // console.log(responseSpanish[0].hwi.prs[0].mw);
+
+    // // This grabs the first letter of the searched word
+    // console.log("search word ",responseSpanish[0].meta.id)
+    // var firstLetter  = responseSpanish[0].meta.id.slice(0, 1);
+    // console.log("letter ",firstLetter)
+    // // Audio recording of the word
+    // var verbalWord = $('<audio controls>');
+    // var src  =  $('<source>').attr("src", "https://media.merriam-webster.com/soundc11/"+ firstLetter + "/" + responseSpanish[0].hwi.prs[0].sound.audio + ".wav");
+    // verbalWord.append(src)
+    // console.log(responseSpanish[0].hwi.prs[0].sound.audio);
+    
+    // // First definition of the word
+    // var wordDefinition1 = $('<h5>').text("1." + responseSpanish[0].shortdef[0]);
+    // console.log(responseSpanish[0].shortdef[0]);
+    
+    // // Second definition of the word
+    // var wordDefinition2 = $('<h5>').text("2." + responseSpanish[0].shortdef[1]);
+    // console.log(responseSpanish[0].shortdef[1]);
+
+    // $(".headword-entry").empty();
+    // $(".headword-entry").append(searchedSpanishWord, sayWord, verbalWord, wordDefinition1, wordDefinition2)
+    // })
+
+
+
+
     // querying giphy api for the user input headword - needs edits to pull exactly the number of gifs wanted (all gifs?)
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + headword + "&api_key=hWQiEMajiXFk4g6MjVMs6vw3DMs9MesS&limit&limit=50";
+    var giphyURL = "http://api.giphy.com/v1/gifs/search?q=" + headword + "&api_key=hWQiEMajiXFk4g6MjVMs6vw3DMs9MesS&limit&limit=36";
     
     $.ajax({
-      url: queryURL,
+      url: giphyURL,
       method: "GET"
 
       
@@ -55,13 +103,17 @@ function searchDictionaryAndGiphy(headword) {
 
       var giphyOne = response.data[0].images.original.url;
 
-      $('#firstGiph').attr("src", giphyOne).css({"width": "", "height": "400px"});
+      // var giphyTwo = response.data[1].images.original.url;
 
-    for(var i =0; i < 50; i++){
+      $('#firstGiph').attr("src", giphyOne).css({"width": "200px", "height": "200px"});
+      // $('#secondGiph').attr("src", giphyTwo).css({"width": "200px", "height": "200px"});
+
+    for(var i =0; i < 36; i++){
       // For simplicity, we will take the first gif (ie. at postion 0)
       
       var giphyURL = response.data[i].images.original.url;
-      var img = $('<img>').attr("src", giphyURL).css({"width": "", "height": "300px"});
+      var img = $('<img>').attr("src", giphyURL).css({"width": "200px", "height": "200px"});
+
       console.log(giphyURL)
 
       // add image to div
@@ -92,20 +144,22 @@ function searchDictionaryAndGiphy(headword) {
 
 
   // event handler for user clicking the search button/pressing enter key
-  $("#firstSearchButton").on("click", function(event) {
+
+  $("#mainSearchButton").on("click", function(event) {
     // Preventing the button from trying to submit the form (aka reload/refresh the page)
     event.preventDefault();
     // Storing the headword
-    var inputHeadword = $("#firstSearch").val().trim();
+    var inputHeadword = $("#mainSearch").val().trim();
 
-    $("#bowlDisplay").fadeOut(1000, function(){
+
+    $("#originalSearch").fadeOut(1000, function(){
             
     });
     $("#def-Display").fadeIn(1000, function(){
       
     });
-    $("#pDefDisplay").fadeIn(1000, function(){
-
+    $("#spanishCard").fadeIn(1000, function(){
+      
     });
     $("#barDisplay").fadeIn(1000, function(){
         
@@ -118,7 +172,10 @@ function searchDictionaryAndGiphy(headword) {
     
   });
 
+ 
+
 
 });
+
 
 
